f6e80d776ffd7dc8a38436479c089c6c14678116
Praveen <praveensankar000@gmail.com>
2026-04-07 12:56:56 +0530

    Separated out dependencies from test and the run scripts

--- run.sh
+++ run.sh
@@ -2,9 +2,6 @@
 
 # Start Hardhat node in background
 cd blockchain-hardhat/
-npm install --save-dev hardhat
-npm install --save-dev @nomicfoundation/hardhat-toolbox
-npm install --save-dev @nomicfoundation/hardhat-ignition-ethers
 npx hardhat node &
 
 # Wait a bit for Hardhat to start
@@ -12,9 +9,7 @@ sleep 2
 
 # Start Go app (foreground)
 cd ../
-go install github.com/ethereum/go-ethereum/cmd/abigen@latest
 go run zkrevoke
 
 cd plots/
-poetry install --no-root
 poetry run python main.py
